a72600e958934c93e2b2f12116b481d0727ca2da
Krunali <krunalilole29@gmail.com>
2022-05-30 02:24:59 +0530

    Adding Hello World Web and Rest controller program

---
+++ helloworld/helloworld/src/main/java/com/bridgelabz/helloworld/controller/HelloWebController.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import com.bridgelabz.helloworld.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@Controller
+@ResponseBody
+public class HelloWebController
+{
+        @GetMapping("/web")
+        public String hello(){
+            return "hello";
+        }
+        @GetMapping("/web/message")
+        public String message (Model model) {
+            model.addAttribute("message","This is a custom message. Hello from BridgeLabz");
+            return "message";
+        }
+}
