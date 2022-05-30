7b84dddd0e1cbea702d92b46506ac30be0c5d0cb
Krunali <krunalilole29@gmail.com>
2022-05-30 21:33:51 +0530

    Adding UC-2 to Use GET Req method and pass query parameter

--- hellomessagingapp/hellomessagingapp/src/main/java/com/bridgelabz/hellomessagingapp/controller/HelloRestController.java
+++ hellomessagingapp/hellomessagingapp/src/main/java/com/bridgelabz/hellomessagingapp/controller/HelloRestController.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.hellomessagingapp.controller;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +16,14 @@ public class HelloRestController {
     }
 
     @RequestMapping(value = {"/hello -w"}, method = RequestMethod.GET)
-    public String sayHelloDifferently() {
+    public String sayHelloDifferently()
+    {
         return "Hello  from BridgeLabz !!!";
+    }
+    //* UC -2 --------------------->
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name)
+    {
+        return "Hello " + name  + " from Bridgelabz !!!";
     }
 }
